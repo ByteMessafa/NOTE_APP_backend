12a9fa78886364bc4cd58889dc1934b540ee8ca3
Messafa Mohammed <172132016926@etu.univ-chlef.dz>
2023-11-20 23:16:17 +0100

    v0.0.0

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -1,5 +1,17 @@
+const express = require('express');
+const generator = require('../Util/generator');
+var store = require('../Util/memoryStorage');
+
+
 exports.getAddNotes = (req, res) => {
-    res.send('get all notes');
+    // const new_id_1 = generator.generate();
+    store.setItem('0001', 'Hello MemoryStorage!');
+    // const new_id_2 = generator.generate();
+    store.setItem('0002', 'Hello MemoryStorage!');
+    const keys = store.getKeys(store);
+    const values = store.getValues(store);
+    console.log('keys: ' + JSON.stringify(keys));
+    res.send(`storage: ${JSON.stringify(keys)}${JSON.stringify(values)}`);
 };
 
 exports.saveNotes = (req, res) => {
@@ -9,6 +21,7 @@ exports.saveNotes = (req, res) => {
 exports.updateNotes = (req, res) => {
     res.send('update a note');
 };
+
 
 exports.deleteNotes = (req, res) => {
     res.send('delete a note');
